065ebf630357f873fa579167fd3aae770dcc01c6
fajritaridala <fadjritaridala@gmail.com>
2026-05-02 20:21:15 +0800

    fix: perubahan dari axios ke fetch

--- src/utils/libs/axios/instance.ts
+++ src/utils/libs/axios/instance.ts
@@ -1,23 +1,22 @@
-import type { AxiosInstance } from 'axios'
-import axios from 'axios'
-import { API_URL } from '../../env'
+// import type { AxiosInstance } from 'axios'
+// import axios from 'axios'
 
-const api: AxiosInstance = axios.create({
-  baseURL: API_URL,
-  timeout: 10000,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   Accept: 'application/json',
-  // },
-})
+// const api: AxiosInstance = axios.create({
+//   baseURL: ,
+//   timeout: 10000,
+//   // headers: {
+//   //   'Content-Type': 'application/json',
+//   //   Accept: 'application/json',
+//   // },
+// })
 
-api.interceptors.response.use(
-  (response) => response.data,
-  (error) => {
-    console.error(`[API Error]: ${error.message}`)
+// api.interceptors.response.use(
+//   (response) => response.data,
+//   (error) => {
+//     console.error(`[API Error]: ${error.message}`)
 
-    return Promise.reject(error)
-  }
-)
+//     return Promise.reject(error)
+//   }
+// )
 
-export default api
+// export default api
