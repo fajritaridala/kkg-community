704437a95e2e50302aaa9a31005f100714b096a8
fajritaridala <fadjritaridala@gmail.com>
2026-05-04 22:21:45 +0800

    feat: menambahkan tombol prev & next di carousel gallery card

--- src/components/Card/Gallery.tsx
+++ src/components/Card/Gallery.tsx
@@ -1,5 +1,7 @@
 import useEmblaCarousel from 'embla-carousel-react'
+import type { EmblaCarouselType } from 'embla-carousel'
 import { useState } from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 
 type PropTypes = {
   src: string[]
@@ -10,12 +12,15 @@ type PropTypes = {
 
 export default function GalleryCard(props: PropTypes) {
   const { src, title, description, date } = props
-  const [emblaRef] = useEmblaCarousel({ align: 'center' })
+  const [emblaRef, emblaApi] = useEmblaCarousel({ align: 'center', loop: true })
   const [isExpanded, setIsExpanded] = useState(false)
 
+  const goToPrev = () => (emblaApi as EmblaCarouselType)?.scrollPrev()
+  const goToNext = () => (emblaApi as EmblaCarouselType)?.scrollNext()
+
   return (
-    <div className="mb-4 h-fit basis-full rounded-lg pb-4 shadow-sm">
-      <div className="relative cursor-grab overflow-hidden p-2">
+    <div className="mb-4 h-fit basis-full rounded-lg pb-4 shadow-sm md:shadow-none">
+      <div className="group relative cursor-grab overflow-hidden p-2">
         <div ref={emblaRef} className="overflow-hidden rounded-lg">
           <div className="flex basis-full touch-pan-y touch-pinch-zoom rounded-lg">
             {src.map((img, index) => (
@@ -34,6 +39,23 @@ export default function GalleryCard(props: PropTypes) {
               </div>
             ))}
           </div>
+        </div>
+
+        <div className="absolute top-0 hidden h-full w-1 items-center-safe md:flex">
+          <button
+            onClick={goToPrev}
+            className="ml-2 cursor-pointer rounded-full p-1 ring-gray/30 duration-75 active:bg-gray/50 active:ring-4"
+          >
+            <ChevronLeftIcon className="size-8 rounded-full bg-gray/40 p-1 text-dark/80 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 hover:bg-gray/60 hover:text-dark/60" />
+          </button>
+        </div>
+        <div className="absolute top-0 right-2 hidden h-full w-12 items-center-safe md:flex">
+          <button
+            onClick={goToNext}
+            className="cursor-pointer rounded-full p-1 ring-gray/30 duration-75 active:bg-gray/50 active:ring-4"
+          >
+            <ChevronRightIcon className="size-8 rounded-full bg-gray/40 p-1 text-dark/80 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 hover:bg-gray/60 hover:text-dark/60" />
+          </button>
         </div>
       </div>
       <div className="space-y-1 px-4">
